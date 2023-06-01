f986dc871ae2262abcaa04f953900e13a51f7a5d
Minh Vu <ngocminhvu883@gmail.com>
2023-06-02 01:24:42 +0700

    prettier

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -2,16 +2,16 @@ const { SlashCommandBuilder } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
-	category: 'fun',
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply({ content: 'Hee!', ephemeral: true });
-		await wait(2000);
-		await interaction.editReply({
-			content: 'Haw!',
-			ephemeral: true,
-		});
-	},
+    category: 'fun',
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        await interaction.reply({ content: 'Hee!', ephemeral: true });
+        await wait(2000);
+        await interaction.editReply({
+            content: 'Haw!',
+            ephemeral: true
+        });
+    }
 };
